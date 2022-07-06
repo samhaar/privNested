9def43b11435e3b99a0be0ca15df2397d2b2b173
samhaar <68865950+samhaar@users.noreply.github.com>
2022-07-06 17:06:41 -0400

    Update localStorage.py

--- localStorage.py
+++ localStorage.py
@@ -4,6 +4,8 @@ st.components.v1.html("""
 <input type="text" id="myText">
 <button id="setButton">set it</button>
 <button id="getButton">get it</button>
+<button id="setC">set cookie</button>
+<button id="getC">get cookie</button>
 <script>
     const set = () => {
         const txt = document.getElementById('myText').value
@@ -14,8 +16,19 @@ st.components.v1.html("""
         alert(localStorage.getItem('bob'))
     }
     
+    const setC = () => {
+        const txt = document.getElementById('myText').value
+        document.cookie = `bob=${txt}; sameSite=none; secure=true; path=/`
+    }
+    
+    const getC = () => {
+        alert(document.cookie)
+    }
+    
     
     document.getElementById('setButton').addEventListener('click', set)
     document.getElementById('getButton').addEventListener('click', get)
+    document.getElementById('setC').addEventListener('click', setC)
+    document.getElementById('getC').addEventListener('click', getC)
 </script>
 """)
